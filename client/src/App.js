c0def41a3f1b2e179997400a0b3c66604abee1a9
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-03-04 14:27:29 -0600

    made each movie card a link

--- client/src/App.js
+++ client/src/App.js
@@ -22,9 +22,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div> 
         <Route exact path={`/`} component={MovieList} /> 
-        <Route /> 
+        <Route path={`/movies/:id`} component={Movie} /> 
       </div>
     );
   }
